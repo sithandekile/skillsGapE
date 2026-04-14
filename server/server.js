13495b9262cbffaf9eb37eb4ff54199960c664fc
sithandekile <sthandie15@gmail.com>
2026-04-14 22:39:44 +0200

    adding CORS middleware to allow requests from the frontend application. The origin is set to either the value of the CORS_ORIGIN environment variable or 'http://localhost:5173' by default.

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dbConnection=require('./config/db')
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:5173'}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
